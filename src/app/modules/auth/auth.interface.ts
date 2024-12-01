ed69c689faddc349ac99e9789ebc1b7480404549
mahfujgithub <mahfujurrahmanofficial23@gmail.com>
2024-12-01 20:51:44 +0600

    logout route, update pass, reset pass, forget pass

--- src/app/modules/auth/auth.interface.ts
+++ src/app/modules/auth/auth.interface.ts
@@ -1,4 +1,5 @@
 import { Model } from 'mongoose';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 export type ILoginUser = {
   id: string;
@@ -12,6 +13,16 @@ export type ILoginUserResponse = {
 
 export type IRefreshTokenResponse = {
   accessToken: string;
+};
+
+export type IVerifiedLoginUser = {
+  userId: string;
+  role: ENUM_USER_ROLE;
+};
+
+export type IChangePassword = {
+  oldPassword: string;
+  newPassword: string;
 };
 
 export type AuthModel = Model<ILoginUser, Record<string, unknown>>;
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { AuthValidation } from './auth.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthController } from './auth.controller';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
 router.post(
@@ -15,5 +17,21 @@ router.post(
   validateRequest(AuthValidation.refreshTokenZodSchema),
   AuthController.refreshToken,
 );
+
+router.post(
+  '/change-password',
+  validateRequest(AuthValidation.changePasswordZodSchema),
+  auth(
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.CONTENT_WRITER,
+  ),
+  AuthController.changePassword,
+);
+
+router.post('/forgot-password', AuthController.forgotPass);
+
+router.post('/reset-password', AuthController.resetPassword);
+
+router.post('/logout', AuthController.logout);
 
 export const AuthRoutes = router;
